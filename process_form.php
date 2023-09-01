2e9d0586959bfcb3f96bd877577547b729236bbd
Abiodun Ayo-Bali <ayobali.biodun@gmail.com>
2023-09-01 07:16:10 -0400

    added

--- process_form.php
+++ process_form.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 // Use the mail() function to send an email
-mail('geobalies.datascience@gmail.com', 'New Contact Form Submission', $message, "From: $email");
+mail('mailto:geobalies.datascience@gmail.com', 'New Contact Form Submission', $message, "From: $email");
 
 // Redirect the user to a thank-you page
 header('Location: thank_you.html');
